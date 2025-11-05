fb47adf079db1bab987bc960cf745618aada0bf4
Bilal Ahmed <bilalahmed7072@gmail.com>
2025-11-05 18:01:16 +0500

    Update build_index.py

--- ingestion/build_index.py
+++ ingestion/build_index.py
@@ -1,3 +1,4 @@
+"""
 import os
 import pickle
 import faiss
@@ -10,13 +11,13 @@ INDEX_PATH = "../data/vector_index"
 
 def build_index():
     print("loading documents")
-    docs= load_documents(DOCS_PATH)
+    docs = load_documents(DOCS_PATH)
 
     print("making chunks of documents")
     chunks = chunk_documents(docs)
 
     print("embedding the chunks")
-    #model = SentenceTransformer("all-MiniLM-L6-v2")
+    # model = SentenceTransformer("all-MiniLM-L6-v2")
     model = SentenceTransformer("multi-qa-MiniLM-L6-cos-v1")
     embeddings = model.encode(chunks)
 
@@ -25,11 +26,13 @@ def build_index():
     index = faiss.IndexFlatL2(dimentions)
     index.add(embeddings)
 
-    os.makedirs(INDEX_PATH,exist_ok=True)
-    faiss.write_index(index, os.path.join(INDEX_PATH,"faiss.index"))
-    with open(os.path.join(INDEX_PATH,"chunks.pkl"),"wb") as f:
-        pickle.dump(chunks,f)
+    os.makedirs(INDEX_PATH, exist_ok=True)
+    faiss.write_index(index, os.path.join(INDEX_PATH, "faiss.index"))
+    with open(os.path.join(INDEX_PATH, "chunks.pkl"), "wb") as f:
+        pickle.dump(chunks, f)
 
-    print ("index is built at:", INDEX_PATH)
-if __name__=="__main__":
+    print("index is built at:", INDEX_PATH)
+
+if __name__ == "__main__":
     build_index()
+"""
